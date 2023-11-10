dd614cacf4a2b4d13837e0420dd513fc913915ef
rvahilario <rvahilario@gmail.com>
2023-11-10 19:41:32 -0300

    add: styled-components and global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${(props) => props.theme.background};
+    color: ${(props) => props.theme['base-text']};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.3;
+    -webkit-font-smoothing: antialiased;
+  }
+`
